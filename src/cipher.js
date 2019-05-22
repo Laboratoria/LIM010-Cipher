0e20a6b5386a0f94200610b2377ca99007d2d304
developervilchez <developervilchez@gmail.com>
2019-05-22 15:15:53 -0500

    inicio del proyecto cipher010

---
+++ src/cipher.js
@@ -0,0 +1,8 @@
+window.cipher = {
+  encode: () => {
+    /* Acá va tu código */
+  },
+  decode: () => {
+    /* Acá va tu código */
+  }
+};
---
+++ test/cipher.spec.js
@@ -0,0 +1,25 @@
+describe('cipher', () => {
+
+  it('debería ser un objeto', () => {
+    assert.equal(typeof cipher, 'object');
+  });
+
+  describe('cipher.encode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.encode, 'function');
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+  });
+
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+  });
+
+});
