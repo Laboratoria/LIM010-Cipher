8e6606389af29dff50498afe3347aa4df937f68e
Alejandra Visa <alejandra.visa91@gmail.com>
2019-06-04 00:49:26 -0500

    code & encode function

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,18 @@
+import { strict } from "assert";
+
 window.cipher = {
   encode: (offset, string) => {
     /* Acá va tu código que cifra*/
+    let result = '';
+    for (let indexString = 0; indexString < string.length; indexString++){
+      if (string[indexString] in ['A'..'Z']) {
+        //transformar el caracter usando el offset.
+      } else {
+        result = result+string[indexString];
+      }
+
+    }
+    return result;
   },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
