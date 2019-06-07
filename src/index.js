9b18b01acd23beedf358364e5046fc837459dccb
avisas <alejandra.visa91@gmail.com>
2019-06-07 11:23:46 -0500

    creo la función del pasword y enter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 /* Acá va tu código */
 
+const password = document.getElementById('contraseña'.value);
+const enter = document.getElementById('enter');
 
+enter.addEventListener("click", () => {
+    const password = document.getElementById('contraseña').value;
+    console.log(password)
+}
 function go(){
     if (document.form.password.value=='CONTRASEÑA' && document.form.login.value=='USUARIO'){ 
             document.form.submit(); 
