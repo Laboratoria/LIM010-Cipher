dfa6eb5fcbd1bfdf99d21f14781a83ee71014f86
ElviaRV <elvia-love90@hotmail.com>
2019-06-10 00:36:26 -0500

    cambio a ClassList

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ const contraseña= document.getElementById("contraseña")
 const firstButton = document.getElementById("buttonnext")
 const vistalogin =document.getElementById("login")
 const vistacipher =document.getElementById("cipher")
-const vistacodigo=document.getElementById("codigo")
+
+
 let contador = 0
 document.getElementById("buttonnext").addEventListener("click",()=>{
   if(contraseña.value=="LABORATORIA"){
  
-    vistalogin.style.display="none";
-    vistacipher.style.display="block";
-    vistacodigo.style.display="none"
-    
+    vistacipher.classList.toggle("show")
+    vistalogin.classList.toggle("hide")
   }else if ( contador ==3 ){
     
     document.getElementById("error").innerHTML ="no puede acceder en este momento"} 
