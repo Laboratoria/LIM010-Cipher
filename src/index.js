9a66d63266f602d0f09be2d0c5f66a39adb026d6
ElviaRV <elvia-love90@hotmail.com>
2019-06-09 23:08:34 -0500

    Añadiendo Imagen de Fondo

--- src/index.js
+++ src/index.js
@@ -4,23 +4,19 @@ const firstButton = document.getElementById("buttonnext")
 const vistalogin =document.getElementById("login")
 const vistacipher =document.getElementById("cipher")
 const vistacodigo=document.getElementById("codigo")
+let contador = 0
 document.getElementById("buttonnext").addEventListener("click",()=>{
   if(contraseña.value=="LABORATORIA"){
  
     vistalogin.style.display="none";
     vistacipher.style.display="block";
     vistacodigo.style.display="none"
-    }
-  else{
-    document.getElementById("error").innerHTML ="Contraseña incorrecta"
     
-  } 
-  })
-  function cipher(){
-    vistalogin.style.display="none";
-    vistacipher.style.display="block"
-  }
-  function codigo(){
-    vistalogin.style.display="none";
-    vistacodigo.style.display="block";
-  }
+  }else if ( contador ==3 ){
+    
+    document.getElementById("error").innerHTML ="no puede acceder en este momento"} 
+  else {
+    contador ++
+    document.getElementById("error").innerHTML ="contraseña incorrecta"
+  }})
+ 
