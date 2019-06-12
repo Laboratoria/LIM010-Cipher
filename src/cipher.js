e88990ecfcf0eb0f5ec80ca648311e384223abb7
ElviaRV <elvia-love90@hotmail.com>
2019-06-12 03:10:50 -0500

    corrigiendo comparaciones

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,18 @@
-var mode="cesar"
 window.cipher = {
   encode: (offset, string) => {
-<<<<<<< HEAD
     let cod="";
     for(let i=0;i<string.length;i++) {
       let numascii=string[i].charCodeAt();
-      if (numascii >=65 && numascii<=90){
+      if (numascci >=65 && numascii<=90){
         cod += String.fromCharCode((parseInt(offset)+ numascii - 65 ) % 26 + 65);
       } else {
         cod=string[i];
       }
     }
     return cod;
-=======
-    let result="";
-    if(mode=="cesar"){
-      for(let i=0;i<string.length;i++){
-        let texto =string.chartCodeAt(i);
-        if(texto >= 65 && texto<=90){
-          result += String.fromCharCode((texto-65+offset)% 26 +65);
-        } else if(texto<= 97 && texto<=122){
-          result += String.fromCharCode((c-97+ offset) % 26 +97);
-        }else{
-          result += text.chartCodeAt(i)
-        }
-        }
-        }
-        return result
-      }
-    }
-  
->>>>>>> d9c6d4f1c3c8aa7e930c67ec698f14a54be4bffc
 
   },
   decode: (offset, string) => {
-<<<<<<< HEAD
     let dec = '';
     for (let i = 0; i < string.length; i++) {
       let numascii = string[i].charCodeAt();
@@ -57,8 +35,4 @@ window.cipher = {
     }
   }
 }
-=======
- 
-  }
->>>>>>> d9c6d4f1c3c8aa7e930c67ec698f14a54be4bffc
 
