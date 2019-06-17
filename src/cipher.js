a8583d2f47404e685b2e8f0b3e5be3f3362ee6e7
avisas <alejandra.visa91@gmail.com>
2019-06-17 08:58:45 -0500

    actualicé decode en cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -12,15 +12,18 @@ window.cipher = {
     return result;
   },
   decode: (offset, string) => {
-    let result = '';
-    for (let c of string) {
-      if (c >= 'A' && c <= 'Z') {
-        let ASCIICode = (c.charCodeAt(0) - 65 - offset) % 26 + 65;
-        result += String.fromCharCode(ASCIICode);
+    let decodedString = '';
+    offset = Math.abs(offset) % 26;
+    for (let char of string) {
+      if ((char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z')) {
+        let upperCase = (char >= 'A' && char <= 'Z') ? true : false;
+        let charCode = char.toUpperCase().charCodeAt(0) - 65;
+        let decodedCharCode = (charCode - offset) % 26;
+        decodedCharCode = (decodedCharCode < 0) ? (26 + decodedCharCode) : decodedCharCode;
+        decodedString += String.fromCharCode(upperCase ? decodedCharCode + 65 : decodedCharCode + 97);
       } else {
-        result += c;
+        decodedString += char;
       }
     }
-    return result;
-  }
-};
+    return decodedString;
+  };
