2d50b138a473fb3bc11dda4cd6d326959c63edfb
Elvia <elvia-love90@hotmail.com>
2019-06-19 17:09:47 -0500

    resposive changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const vistacipher = document.getElementById("cipher")
 const botonCifrado = document.getElementById("buttoncifrar")
 const botonDescifrado = document.getElementById("buttondecifrar")
 const limpiar =document.getElementById("clear")
+const error=document.getElementById("error")
+let cajadatos=document.getElementById("Datos")
+let cajacodigo=document.getElementById("Codigo")
+let cajaoffset=document.getElementById("num")
 /*Loop para la funcionalidad de la contraseña*/
 let contador = 0
 firstButton.addEventListener("click", () => {
@@ -15,33 +19,33 @@ firstButton.addEventListener("click", () => {
     vistalogin.classList.toggle("hide")
   } else if (contador == 3) {
 
-    document.getElementById("error").innerHTML = "Ya utilizaste todos tus intentos, en este momento no podrás ingresar"
+   error.innerHTML = "Ya utilizaste todos tus intentos, en este momento no podrás ingresar"
   }
   else {
     contador++
-    document.getElementById("error").innerHTML = "Contraseña incorrecta"
+    error.innerHTML = "Contraseña incorrecta"
   }
 })
 /*Creamos la acciones de los botones anteriormente declarados*/
 
 /*Botón para la funcionalidad de cifrado*/
 botonCifrado.addEventListener("click", () => {
-  let string = document.getElementById("Datos").value.toUpperCase();
-  let offset = parseInt(document.getElementById("num").value);
-  document.getElementById("Codigo").innerHTML = cipher.encode(offset, string);
+  let string = cajadatos.value.toUpperCase();
+  let offset = parseInt(cajaoffset.value);
+  cajacodigo.innerHTML = cipher.encode(offset, string);
 })
 /*Botón para la funcionalidad de descifrado*/
 botonDescifrado.addEventListener("click", () => {
-  let string = document.getElementById("Codigo").value.toUpperCase();
-  let offset = parseInt(document.getElementById("num").value);
-  document.getElementById("Datos").innerHTML = cipher.decode(offset, string);
+  let string = cajacodigo.value.toUpperCase();
+  let offset = parseInt(cajaoffset.value);
+  cajadatos.innerHTML = cipher.decode(offset, string);
 
 })
 /*Botón para la funcionalidad de clear*/
 limpiar.addEventListener("click", (event) => {
-  document.getElementById("Codigo").innerHTML = null;
-  document.getElementById("Datos").value = null;
-  document.getElementById("Codigo").value = null;
-  document.getElementById("Datos").innerHTML = null;
+  cajacodigo.innerHTML = null;
+  cajadatos.value = null;
+  cajacodigo.value = null;
+  cajadatos.innerHTML = null;
   event.preventDefault()
 })
