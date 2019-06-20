87ad81f37a105366f8549967e77c0752a78eb02d
Alejandra Visa <alejandra.visa91@gmail.com>
2019-06-20 08:23:40 -0500

    added comments

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,15 @@
 window.cipher = {
   encode: (offset, string) => {
     let encodedString = '';
-    offset = Math.abs(offset) % 26;
+    offset = Math.abs(offset) % 26;//transformo a positivo y elimino vueltas
     for (let character of string) {
       if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
-        let upperCase = (character >= 'A' && character <= 'Z') ? true : false;
-        let charCode = character.toUpperCase().charCodeAt(0) - 65;
-        let decodedCharCode = (charCode + offset) % 26;
-        encodedString += String.fromCharCode(upperCase ? decodedCharCode + 65 : decodedCharCode + 97);
+        let upperCase = (character >= 'A' && character <= 'Z') ? true : false;//upperCase es True si character es mayuscula... false caso contrario
+        let charCode = character.toUpperCase().charCodeAt(0) - 65;//variable charCode guarda el codigo de 0 a 25 de character
+        let decodedCharCode = (charCode + offset) % 26;//variable decodedCharCode guarda el codigo resultado de 0 a 25
+        encodedString += String.fromCharCode(upperCase ? decodedCharCode + 65 : decodedCharCode + 97);//sumo 65 (Mayusc) o 97 (Minusc) y concateno a encodedString
       } else {
-        encodedString += character;
+        encodedString += character;//concateno a encodedString el character original
       }
     }
     return encodedString;
@@ -17,16 +17,16 @@ window.cipher = {
 
   decode: (offset, string) => {
     let decodedString = '';
-    offset = Math.abs(offset) % 26;
+    offset = Math.abs(offset) % 26;//transformo a positivo y elimino vueltas
     for (let character of string) {
       if ((character >= 'A' && character <= 'Z') || (character >= 'a' && character <= 'z')) {
-        let upperCase = (character >= 'A' && character <= 'Z') ? true : false;
-        let charCode = character.toUpperCase().charCodeAt(0) - 65;
-        let decodedCharCode = (charCode - offset) % 26;
-        decodedCharCode = (decodedCharCode < 0) ? (26 + decodedCharCode) : decodedCharCode;
-        decodedString += String.fromCharCode(upperCase ? decodedCharCode + 65 : decodedCharCode + 97);
+        let upperCase = (character >= 'A' && character <= 'Z') ? true : false;//upperCase es True si character es mayuscula... false caso contrario
+        let charCode = character.toUpperCase().charCodeAt(0) - 65;//variable charCode guarda el codigo de 0 a 25 de character
+        let decodedCharCode = (charCode - offset) % 26;//variable decodedCharCode guarda el codigo resultado de 0 a 25
+        decodedCharCode = (decodedCharCode < 0) ? (26 + decodedCharCode) : decodedCharCode;//si el decodedCharCode es negativo, entonces le sumo 26 para que sea un codigo valido entre 0 a 25
+        decodedString += String.fromCharCode(upperCase ? decodedCharCode + 65 : decodedCharCode + 97);//sumo 65 (Mayusc) o 97 (Minusc) y concateno a encodedString
       } else {
-        decodedString += character;
+        decodedString += character;//concateno a encodedString el character original
       }
     }
     return decodedString;
